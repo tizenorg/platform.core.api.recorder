7e1a40eb406bc75ef1717a566ff7e4900151b585
Jeongmo Yang <jm80.yang@samsung.com>
2015-05-27 15:18:03 +0900

    Update latest code
    Change-Id: I20606833ed7f44f9f534e2272bf960816b49b7e7
    Signed-off-by: Jeongmo Yang <jm80.yang@samsung.com>

--- include/recorder_private.h
+++ include/recorder_private.h
@@ -11,7 +11,7 @@
 * distributed under the License is distributed on an "AS IS" BASIS,
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 * See the License for the specific language governing permissions and
-* limitations under the License. 
+* limitations under the License.
 */
 
 
@@ -33,19 +33,23 @@ typedef union _mediaSource{
 
 typedef enum {
 	_RECORDER_EVENT_TYPE_STATE_CHANGE,
-	_RECORDER_EVENT_TYPE_RECORDING_LIMITED,	
+	_RECORDER_EVENT_TYPE_RECORDING_LIMITED,
 	_RECORDER_EVENT_TYPE_RECORDING_STATUS,
 	_RECORDER_EVENT_TYPE_INTERRUPTED,
-	_RECORDER_EVENT_TYPE_INTERRUPT_COMPLETED,
 	_RECORDER_EVENT_TYPE_AUDIO_STREAM,
 	_RECORDER_EVENT_TYPE_ERROR,
 	_RECORDER_EVENT_TYPE_NUM
 }_recorder_event_e;
 
 typedef enum {
-	_RECORDER_TYPE_AUDIO= 0,
+	_RECORDER_TYPE_AUDIO = 0,
 	_RECORDER_TYPE_VIDEO
 }_recorder_type_e;
+
+typedef enum {
+	_RECORDER_SOURCE_TYPE_UNKNOWN,
+	_RECORDER_SOURCE_TYPE_CAMERA,
+}_recorder_source_type_e;
 
 typedef struct _recorder_s{
 	MMHandleType mm_handle;
@@ -53,13 +57,15 @@ typedef struct _recorder_s{
 	void* user_cb[_RECORDER_EVENT_TYPE_NUM];
 	void* user_data[_RECORDER_EVENT_TYPE_NUM];
 	unsigned int state;
-	_recorder_type_e  type;
+	int camera_device_count;
+	_recorder_type_e type;
+	_recorder_source_type_e src_type;
 	int origin_preview_format;
 	int changed_preview_format;
 	double last_max_input_level;
 } recorder_s;
 
-static int __convert_recorder_error_code(const char *func, int code);
+int __convert_recorder_error_code(const char *func, int code);
 #ifdef __cplusplus
 }
 #endif
